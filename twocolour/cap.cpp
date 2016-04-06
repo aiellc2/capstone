c5d58c4a47fc4215f8b3bbfe80c5e53719610f3e
Chris <aiellocs@mcmaster.ca>
2016-04-06 14:05:16 -0400

    added mouse movement with green. todo: add clicking

--- twocolour/cap.cpp
+++ twocolour/cap.cpp
@@ -4,23 +4,71 @@
 #include <semaphore.h>
 #include <queue>
 #include <cmath>
+#include <stdlib.h>
+#include <stdio.h>
+
 
 #define GLOBAL_COLS 640
 #define GLOBAL_ROWS 360
 
+#define SCREEN_RES_HOR 1440
+#define SCREEN_RES_VER 900
+
+#define res_mapx(X) ((X * SCREEN_RES_HOR / GLOBAL_COLS)) //map 640x360 to screen resolution
+#define res_mapy(X) (X * SCREEN_RES_VER / GLOBAL_ROWS)
+
+
+
 
 using namespace cv;
 
-//Mat capBuffer[256];
 std::queue <Mat> capBuffer;
 int bufIndex = 0;
 int procIndex = 0;
 Mat tempCap;
 pthread_mutex_t mutex;
 sem_t *s1;
+sem_t *s2;
+
+struct mouseMove{
+  int x;
+  int y;
+};
+
+struct mouseMove mouse;
 
 VideoCapture capture(0); // open the default camera
 
+
+void *mouseControl(void *threadid){
+  int x_avg = 0;
+  int y_avg = 0;
+  int count = 0;
+  while(1){
+    //printf("mouse wait\n");
+    sem_wait(s2); //wait for signal
+    //printf("mouse move\n");
+    //printf("mouse.x %d\n",mouse.x);
+    x_avg += mouse.x;
+    y_avg += mouse.y;
+
+    count++;
+
+
+    if (count == 2){
+
+      count = 0;
+
+      char cmd[64];
+      sprintf(cmd, "cliclick m:%d,%d", x_avg/2, y_avg/2); //use average for smoothing
+      system(cmd);
+
+      x_avg = 0;
+      y_avg = 0;
+    }
+  }
+
+}
 
 void *cap(void *threadid) {
     std::cout << "1this is cap\n";
@@ -38,7 +86,7 @@ void *cap(void *threadid) {
             printf("buf getting full...\n");
         }
 
-        usleep(160000); //increase delay if computer can not keep
+        usleep(100000); //increase delay if computer can not keep
         //usleep  (160000);
         //return 0;
     }
@@ -50,49 +98,48 @@ int main(int argc, char **argv) {
     // Check VideoCapture documentation.
     pthread_mutex_init(&mutex, NULL);
     namedWindow("im1", 0);
-    pthread_t thread[1];
+    pthread_t thread[2];
     int rc;
     long t;
     sem_unlink("sem1");
-    s1 = sem_open("sem1", O_CREAT, 0777, 0);
+    sem_unlink("sem2");
 
-    rc = pthread_create(&thread[0], NULL, cap, (void *) t); //creates threads to run sort()
+    s1 = sem_open("sem1", O_CREAT, 0777, 0);
+    s2 = sem_open("sem2", O_CREAT, 0777, 0);
+
+
+    rc = pthread_create(&thread[0], NULL, cap, (void *) t); //creates threads to run video capture
+    rc = pthread_create(&thread[1], NULL, mouseControl, (void *) t); //creates thread to run mouseControl
 
     Mat frame;
     Mat im_HSV, im_HSV2;
     Mat imgThresholded, imgThresholded2;
 
-    //Mat diagH = Mat::zeros(200,200, CV_8UC3 );;
-    //Mat diagBGR;
-  //  Mat diagS = Mat::zeros(150,150, CV_8UC1 );;
-  //  Mat diagV = Mat::zeros(150,150, CV_8UC1 );;
-
-
     namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
     namedWindow("Control2", CV_WINDOW_AUTOSIZE); //create a window called "Control"
 
     //namedWindow("HuePreview", CV_WINDOW_AUTOSIZE);
-    //114 141 75 161 255 255 default green
+    //114 141 75 161 255 255 default red
 
-    //36 86 101 88 255 255 default red
+    //36 86 101 88 255 255 default green
 
-    int iLowH = 36;
-    int iHighH = 88;
+    int iLowH2 = 10;
+    int iHighH2 = 88;
 
-    int iLowS = 86; //reds
-    int iHighS = 255;
-
-    int iLowV = 101;
-    int iHighV = 255;
-
-    int iLowH2 = 114;
-    int iHighH2 = 151;
-
-    int iLowS2 = 141;   //greens
+    int iLowS2 = 86; //greens
     int iHighS2 = 255;
 
-    int iLowV2 = 75;
+    int iLowV2= 101;
     int iHighV2 = 255;
+
+    int iLowH = 114;
+    int iHighH = 151;
+
+    int iLowS = 141;   //reds
+    int iHighS = 255;
+
+    int iLowV = 75;
+    int iHighV = 255;
 
     int gestureSensitivity=(275-100);
 
@@ -150,9 +197,6 @@ int main(int argc, char **argv) {
       capBuffer.pop();
 
       pthread_mutex_unlock(&mutex);
-
-      //diagH.setTo(Scalar(iLowH,255,255));
-      //cvtColor(diagH,diagBGR, COLOR_HSV2BGR);
 
       cvtColor(frame,im_HSV,COLOR_RGB2HSV);
       im_HSV2 = im_HSV;
@@ -262,7 +306,7 @@ int main(int argc, char **argv) {
             shrink++;
             grow=0;
             lastDist = abs(posX - posX2);
-            if(shrink >= 6){
+            if(shrink >= 5){
               shrink = 0;
               printf("SHRINK\n");
               system("osascript -e 'activate application \"iBooks\"'");
@@ -290,6 +334,30 @@ int main(int argc, char **argv) {
 
         iLastX2 = posX2;
         iLastY2 = posY2;
+
+      }
+
+      else if(dArea2 > 10000 && dArea <1000){ //if only green
+        //mouse Control
+        //printf("Mouse!\n");
+
+        int posX2 = dM102 / dArea2;
+        int posY2 = dM012 / dArea2;
+
+        //iLastX2 = posX2;
+        //iLastY2 = posY2;
+
+        //printf("posx %d\n",posX2);
+        posX2 = posX2 > (GLOBAL_COLS - 15) ? posX2+7 : posX2;
+        posX2 = posX2 < (GLOBAL_COLS - (GLOBAL_COLS-15)) ? posX2-7 : posX2;
+
+        posY2 = posY2 > (GLOBAL_ROWS - 15) ? posY2+7 : posY2;
+        posY2 = posY2 < (GLOBAL_ROWS - (GLOBAL_ROWS-15)) ? posY2-7 : posY2;
+
+        mouse.x = res_mapx((GLOBAL_COLS - posX2));
+        mouse.y = res_mapy(posY2);
+
+        sem_post(s2); //signal mousecontrol to proceed
 
       }
 
